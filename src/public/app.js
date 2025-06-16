20eb588789c3dfae7c4acaaf8533067ac3d7c9f5
Isaac-Cheong <24045477@myrp.edu.sg>
2025-06-16 20:31:41 +0800

    Initial commit

--- src/public/app.js
+++ src/public/app.js
@@ -163,7 +163,7 @@ if (contactForm) {
             return;
         }
 
-        alert('Thank you for your message! We will get back to you soon.');
+        alert('Thank you for your message! We really appreciate taking the time to contact us.');
         contactForm.reset();
     });
 }
